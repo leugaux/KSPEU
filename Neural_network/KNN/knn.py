98787ffed97e59863f0666eb9dd081c98377fe0d
Eugene <64373502+leugaux@users.noreply.github.com>
2020-04-26 23:26:50 +0300

    Create knn.py

---
+++ Neural_network/KNN/knn.py
@@ -0,0 +1,7 @@
+#Импорт пакетов и зависимостей
+import numpy as np
+import mnist #Набор данных MNIST
+import matplotlib.pyplot as plt #График
+from keras.models import Sequential #Архитектура нейросети
+from keras.layers import Dense #Слой в нейросети
+from keras.utils import to_categorical
