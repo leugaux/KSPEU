b242f9d308773dc674d0018011926b5c9f76858d
Eugene <64373502+leugaux@users.noreply.github.com>
2020-04-26 23:35:46 +0300

    Update knn.py

--- Neural_network/KNN/knn.py
+++ Neural_network/KNN/knn.py
@@ -5,3 +5,5 @@ import matplotlib.pyplot as plt #График
 from keras.models import Sequential #Архитектура нейросети
 from keras.layers import Dense #Слой в нейросети
 from keras.utils import to_categorical
+
+import train.py
