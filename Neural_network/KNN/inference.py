f22578f2fbe381125b18c7ba5da0532f536c8b6d
Eugene <64373502+leugaux@users.noreply.github.com>
2020-04-26 23:24:28 +0300

    Update inference.py

--- Neural_network/KNN/inference.py
+++ Neural_network/KNN/inference.py
@@ -1,3 +1,9 @@
+#inference.py
+#Прогнозирование на первых 5 тестовых изображениях
+predictions = model.predict(test_images[9500:9505])
+print( np.argmax(predictions, axis = 1) )
+print( test_labels[9500:9505] )
+
 for i in range(9500,9505):
   first_image = test_images[i]
   first_image = np.array(first_image, dtype = 'float')
