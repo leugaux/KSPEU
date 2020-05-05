3a04f5b9c61e38e5e3f2e92733089dc33633459e
Eugene <64373502+leugaux@users.noreply.github.com>
2020-05-06 00:42:37 +0300

    Delete knn.py

--- Neural_network/KNN/knn.py
+++
@@ -1,8 +0,0 @@
-#Импорт пакетов и зависимостей
-import numpy as np
-import mnist #Набор данных MNIST
-import matplotlib.pyplot as plt #График
-from keras.models import Sequential #Архитектура нейросети
-from keras.layers import Dense #Слой в нейросети
-from keras.utils import to_categorical
-
